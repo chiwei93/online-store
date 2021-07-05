0f169a61520dc51e08671a8060213ecc04387b86
chiwei93 <chiwei0708@gmail.com>
2021-07-05 15:14:47 +0800

    change email settings

--- util/email.js
+++ util/email.js
@@ -3,21 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
-
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take our messages');
-  }
-});
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.SENDGRID_API_KEY,
+    },
+  })
+);
 
 module.exports = transporter;
